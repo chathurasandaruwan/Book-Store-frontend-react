3e8fe79b50c00dacc4ead04a0df21a657b2294d4
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-02-16 19:09:20 +0530

    set header and background colour in Categories.tsx page

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,5 +1,12 @@
+import Footer from "../components/Footer.tsx";
+
 export function Categories() {
     return (
-        <>Categories</>
+        <section className='bg-gray-200'>
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-4xl font-bold mb-8">Categories</h1>
+            </div>
+            <Footer/>
+        </section>
     );
 }
