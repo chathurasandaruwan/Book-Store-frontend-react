d0555ffed410cefb455d6dc9557f5e784f796a86
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-02-21 23:19:49 +0530

    set loading in RootLayout.tsx

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router"
-import { useLocation } from "react-router"
 import {NaviBar} from "./NaviBar.tsx";
+import {useSelector} from "react-redux";
+import {RootState} from "../store/Store.ts";
+import {LoadingAnimation} from "./LoadingAnimation.tsx";
+import {useEffect, useState} from "react";
 
 export function RootLayout() {
-    const location = useLocation()
-
-    console.log(location.pathname)
+    const [loading,setLoading] = useState(false);
+    const isLoadingBook: boolean = useSelector((state:RootState) => state.bookData.loading);
+    useEffect(() => {
+        setLoading(isLoadingBook);
+    }, [isLoadingBook]);
     return (
         <>
             <NaviBar/>
+            {loading && <LoadingAnimation/>}
             <Outlet />
       </>
     )
